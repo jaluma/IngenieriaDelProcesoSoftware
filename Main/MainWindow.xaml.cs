c25efac815d9249ac20e32647723cf6f60b0ef71
Javier Martinez Alvarez <UO258092@uniovi.es>
2018-10-03 18:17:08 +0200

    cambios en la definicion  (ui)

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1 {
+namespace Main {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
--- Main/Properties/Resources.Designer.cs
+++ Main/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WpfApp1.Properties {
+namespace Main.Properties {
 
 
     /// <summary>
