f93f4cc9ade5bc795772b36d2553e4393019bb98
Gema Rico Pozas <UO238096@uniovi.es>
2018-10-11 12:32:34 +0200

    arreglado problema con tipo de carrera

---
+++ Logic.Db/ActionObjects/CompetitionLogic/ListOpenCompetitionLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using Logic.Db.Connection;
+using Logic.Db.Dto;
+
+namespace Logic.Db.ActionObjects.CompetitionLogic
+{
+    class ListOpenCompetitionLogic : IActionObject
+    {
+        private readonly DBConnection _conn;
+        public readonly DataTable Table;
+
+        public ListOpenCompetitionLogic(ref DBConnection conn)
+        {
+            _conn = conn;
+            Table = new DataTable();
+        }
+        public void Execute()
+        {
+            try
+            {
+                using (SQLiteCommand command = new SQLiteCommand(Logic.Db.Properties.Resources.SQL_SELECT_COMPETITION_STATUS, _conn.DbConnection))
+                {
+                    command.Parameters.AddWithValue("@STATUS", "");
+
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(command);
+                    da.Fill(Table);
+                }
+            }
+            catch (SQLiteException)
+            {
+                _conn.DbConnection?.Close();
+                throw;
+            }
+        }
+    }
+}
+
+
+
+
