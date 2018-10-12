3f7a9db9e27fd8361d1a1efc7471dfad47d33824
Javier Martinez Alvarez <UO258092@uniovi.es>
2018-10-13 01:26:54 +0200

    fix connection db

--- Logic.Db/Connection/DBConnection.cs
+++ Logic.Db/Connection/DBConnection.cs
@@ -9,14 +9,13 @@ namespace Logic.Db.Connection {
         private static readonly string SourceFile = System.IO.Path.Combine(Logic.Db.Properties.Resources.DbPath, Logic.Db.Properties.Resources.DbFileName);
         private static readonly string DestFile = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) ?? throw new InvalidOperationException(), Logic.Db.Properties.Resources.DbFileName);
 
-        private SQLiteConnection _dbConnection;
+        private static SQLiteConnection _dbConnection;
 
         public SQLiteConnection DbConnection {
             get {
                 if (_dbConnection == null) {
-                    DBConnection.Inicialize();
-
                     _dbConnection = new SQLiteConnection($"Data Source={Logic.Db.Properties.Resources.DbFileName};Version=3;Pooling=True;Max Pool Size=1500;");
+                    
                     //_dbConnection.Open();
                 }
 
@@ -24,15 +23,17 @@ namespace Logic.Db.Connection {
             }
         }
 
+        public DBConnection() {
+            DBConnection.Inicialize();
+        }
+
         private static void Inicialize() {
             if (!DbIsCreated()) {
                 // copy database.db resource to execution path
-                try {
-                    File.WriteAllBytes(DestFile.Substring(8), Logic.Db.Properties.Resources.Database);
-                } catch (IOException) { }
+                File.WriteAllBytes(DestFile.Substring(8), Logic.Db.Properties.Resources.Database);
             }
         }
 
-        private static bool DbIsCreated() => File.Exists(DestFile);
+        private static bool DbIsCreated() => File.Exists(DestFile.Substring(8));
     }
 }
