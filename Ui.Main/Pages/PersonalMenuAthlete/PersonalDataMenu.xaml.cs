4dc90c878adc76a23601d077ecead42b8bd2cb76
Gema Rico Pozas <UO238096@uniovi.es>
2018-10-15 16:09:18 +0200

    Area personal acabada

--- Ui.Main/Pages/PersonalMenuAthlete/PersonalDataMenu.xaml.cs
+++ Ui.Main/Pages/PersonalMenuAthlete/PersonalDataMenu.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -18,6 +19,7 @@ using Logic.Db.Util;
 using Logic.Db.Util.Services;
 using Logic.Db.Properties;
 
+
 namespace Ui.Main.Pages.PersonalMenuAthlete
 {
     /// <summary>
@@ -25,25 +27,32 @@ namespace Ui.Main.Pages.PersonalMenuAthlete
     /// </summary>
     public partial class PersonalDataMenu : Page
     {
-     
-      
+
+
         private AthletesService _serviceAthlete;
+        private AthletesService _serviceAthleteResult;
         private CompetitionService _serviceComp;
+        private DataTable _tablePersonal;
+        private DataTable _tableInscripcion;
+        private DataTable _tableResult;
+
 
         public PersonalDataMenu()
         {
             InitializeComponent();
-            GeneratePersonalDataTable();
-            GenerateInscriptionsDataTable();
+
         }
 
         private void BtSearch_Click(object sender, RoutedEventArgs e)
         {
+
+
             if (!ComprobarDNI(Dni.Text))
             {
                 MessageBox.Show(Properties.Resources.InvalidDNI);
                 return;
             }
+            _serviceAthlete = new AthletesService();
             DataTable table = _serviceAthlete.SelectAthleteByDni(Dni.Text.ToUpper());
             if (table.Rows.Count == 0)
             {
@@ -52,43 +61,122 @@ namespace Ui.Main.Pages.PersonalMenuAthlete
             }
 
             GeneratePersonalDataTable();
-           
+
+
         }
 
         private void GeneratePersonalDataTable()
         {
-            _serviceAthlete = new AthletesService();           
+            _serviceAthlete = new AthletesService();
 
-            DataTable table = _serviceAthlete.SelectAthleteByDni(Dni.Text.ToUpper());
-           
-            table.Columns[0].ColumnName = Properties.Resources.AthleteDni;
-            table.Columns[1].ColumnName = Properties.Resources.AthleteName;
-            table.Columns[2].ColumnName = Properties.Resources.AthleteSurname;
-           
+            _tablePersonal = _serviceAthlete.SelectAthleteByDni(Dni.Text.ToUpper());
 
-            DataGridDataPersonal.ItemsSource = table.DefaultView;
+            _tablePersonal.Columns[0].ColumnName = Properties.Resources.AthleteDni;
+            _tablePersonal.Columns[1].ColumnName = Properties.Resources.AthleteName;
+            _tablePersonal.Columns[2].ColumnName = Properties.Resources.AthleteSurname;
+            _tablePersonal.Columns[3].ColumnName = Properties.Resources.Competition;
+            _tablePersonal.Columns[4].ColumnName = Properties.Resources.AthleteGender;
+
+
+            for (int i = 1; i < _tablePersonal.Rows.Count; i++) {
+                _tablePersonal.Rows.RemoveAt(i); }
+            if (DataGridResults.Columns.Count >= 1){
+                DataGridResults.Columns.ElementAt(3).Visibility = Visibility.Collapsed;
+            }
+            DataGridDataPersonal.ItemsSource = _tablePersonal.DefaultView;
             GenerateInscriptionsDataTable();
+
+
         }
 
         private void GenerateInscriptionsDataTable()
         {
             _serviceComp = new CompetitionService();
 
-            DataTable table = _serviceComp.SelectAllCompetitionsInscripted(Dni.Text.ToUpper());
-            table.Columns[0].ColumnName = Properties.Resources.Competition_Name;
-            table.Columns[1].ColumnName = Properties.Resources.Competition_Status;
-            table.Columns[2].ColumnName = Properties.Resources.Competition_Date;
-            table.Columns[3].ColumnName = Properties.Resources.AthleteDorsal;
-           
-           
-            DataGridInscriptions.ItemsSource = table.DefaultView;
+            _tableInscripcion = _serviceComp.SelectAllCompetitionsInscripted(Dni.Text.ToUpper());
+            _tableInscripcion.Columns[0].ColumnName = Properties.Resources.Competition;
+            _tableInscripcion.Columns[1].ColumnName = Properties.Resources.Competition_Status;
+            _tableInscripcion.Columns[2].ColumnName = Properties.Resources.Competition_Date;
+            _tableInscripcion.Columns[3].ColumnName = Properties.Resources.AthleteDorsal;
+
+            DataGridInscriptions.ItemsSource = _tableInscripcion.DefaultView;
+            GenerateResultsDataTable();
 
         }
 
         private void GenerateResultsDataTable()
-        {
-           
 
+        {
+            _serviceAthleteResult = new AthletesService();
+            _tableResult = new DataTable();
+
+            DataColumn column = new DataColumn(Properties.Resources.AthletePosition, typeof(string))
+            {
+                AllowDBNull = true
+            };
+            _tableResult.Columns.Add(column);
+
+            _tableResult.Columns[Properties.Resources.AthletePosition].AutoIncrement = true;
+            _tableResult.Columns[Properties.Resources.AthletePosition].AutoIncrementSeed = 1;
+            _tableResult.Columns[Properties.Resources.AthletePosition].AutoIncrementStep = 1;
+
+            _tableResult.Merge(_serviceAthleteResult.SelectAthleteByDni(Dni.Text.ToUpper()));
+
+            _tableResult.Columns[1].ColumnName = Properties.Resources.AthleteDni;
+            _tableResult.Columns[2].ColumnName = Properties.Resources.AthleteName;
+            _tableResult.Columns[3].ColumnName = Properties.Resources.AthleteSurname;
+            _tableResult.Columns[4].ColumnName = Properties.Resources.Competition;
+            _tableResult.Columns[5].ColumnName = Properties.Resources.AthleteGender;
+
+
+            DataGridResults.ItemsSource = _tableResult.DefaultView;
+            if (DataGridResults.Columns.Count >= 1)
+            {
+                DataGridResults.Columns.ElementAt(1).Visibility = Visibility.Collapsed;
+                DataGridResults.Columns.ElementAt(2).Visibility = Visibility.Collapsed;
+                DataGridResults.Columns.ElementAt(3).Visibility = Visibility.Collapsed;
+                DataGridResults.Columns.ElementAt(5).Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CheckBox_OnClick(object sender, RoutedEventArgs e)
+        {
+            _tableResult.DefaultView.RowFilter = GenerateFilter();
+            DataGridResults.ItemsSource = _tableResult.DefaultView;
+        }
+
+        private string GenerateFilter()
+        {
+            string filter = string.Empty;
+
+            if (MaleIsChecked() && FemaleIsChecked())
+            {
+                filter = null;
+            }
+            else if (MaleIsChecked())
+            {
+                filter = $" {Properties.Resources.AthleteGender} = 'M'";
+            }
+            else if (FemaleIsChecked())
+            {
+                filter = $"{Properties.Resources.AthleteGender}= 'F'";
+            }
+            else
+            {
+                filter = $"{Properties.Resources.AthleteGender} <> 'F' and {Properties.Resources.AthleteGender} <> 'M'";
+            }
+
+            return filter;
+        }
+
+        private bool MaleIsChecked()
+        {
+            return (bool)MaleCheckBox.IsChecked;
+        }
+
+        private bool FemaleIsChecked()
+        {
+            return (bool)FemaleCheckBox.IsChecked;
         }
 
         private bool ComprobarDNI(string dni)
@@ -110,11 +198,27 @@ namespace Ui.Main.Pages.PersonalMenuAthlete
         {
             if (e.PropertyType == typeof(System.DateTime))
                 ((DataGridTextColumn)e.Column).Binding.StringFormat = "dd/MM/yyyy";
+
+
+
         }
 
-        private void DataGridDataPersonal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Control component)
+                component.Cursor = Cursors.Hand;
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Control component)
+                component.Cursor = null;
+        }
+
+        private void DataGridResults_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
         {
 
         }
     }
 }
+
