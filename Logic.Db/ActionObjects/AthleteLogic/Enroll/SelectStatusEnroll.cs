bdc6ce1c060b8a527e8ccc4f73a2476386ca89f3
Javier Martinez Alvarez <UO258092@uniovi.es>
2018-11-07 16:48:05 +0100

    bug fix

---
+++ Logic.Db/ActionObjects/AthleteLogic/Enroll/SelectStatusEnroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using Logic.Db.Connection;
+using Logic.Db.Dto;
+
+namespace Logic.Db.ActionObjects.AthleteLogic.Enroll {
+
+    public class SelectStatusEnroll : IActionObject {
+
+        private readonly DBConnection _conn;
+        private readonly CompetitionDto _competition;
+        private readonly string _dni;
+        public string Return;
+
+        public SelectStatusEnroll(ref DBConnection conn, CompetitionDto competitionP, string dni) {
+            _conn = conn;
+            _dni = dni;
+            _competition = competitionP;
+        }
+
+        public void Execute() {
+            try {
+                using (SQLiteCommand command = new SQLiteCommand(Logic.Db.Properties.Resources.SQL_SELECT_ATHLETE_INSCRIPTION, _conn.DbConnection)) {
+                    //command.Parameters.AddWithValue("@STATUS", TypesStatus.Registered.ToString().ToUpper());
+                    command.Parameters.AddWithValue("@COMPETITION_ID", _competition.ID);
+                    command.Parameters.AddWithValue("@DNI", _dni);
+                    using (SQLiteDataReader reader = command.ExecuteReader()) {
+                        if (reader.Read()) {
+                            Return = reader.GetString(0);
+                        }
+                    }
+                }
+            } catch (SQLiteException) {
+                _conn.DbConnection?.Close();
+                throw;
+            }
+        }
+    }
+}
