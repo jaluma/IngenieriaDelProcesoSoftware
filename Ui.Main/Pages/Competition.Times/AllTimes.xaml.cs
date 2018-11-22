5447f146e2010637c5c70e92fc8b3f8b97625c7f
Javier Martinez Alvarez <UO258092@uniovi.es>
2018-11-22 11:46:36 +0100

    bug fix print

--- Ui.Main/Pages/Competition.Times/AllTimes.xaml.cs
+++ Ui.Main/Pages/Competition.Times/AllTimes.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Printing;
 using System.Text;
@@ -20,6 +21,7 @@ using FirstFloor.ModernUI.Windows.Controls;
 using Logic.Db.Dto;
 using Logic.Db.Util.Services;
 using Button = System.Windows.Controls.Button;
+using Cursors = System.Windows.Input.Cursors;
 using DataGrid = System.Windows.Controls.DataGrid;
 using HorizontalAlignment = System.Windows.HorizontalAlignment;
 using Label = System.Windows.Controls.Label;
@@ -38,7 +40,7 @@ namespace Ui.Main.Pages.Competition.Times
         private CompetitionDto _competition;
         private TimesService _service;
 
-        private static string[] Genders = new string[2]{"M", "F"};
+        private static string[] Genders = new string[2] {"M", "F"};
 
         public AllTimes(CompetitionDto competition, IEnumerable<AbsoluteCategory> categories) {
             _competition = competition;
@@ -48,16 +50,18 @@ namespace Ui.Main.Pages.Competition.Times
             InitializeComponent();
 
             //botones
-            Button customButton = new Button() { Content = Properties.Resources.Close, Margin = new Thickness(4) };
+            Button customButton = new Button() {Content = Properties.Resources.Close, Margin = new Thickness(4)};
             customButton.Click += (ss, ee) => { this.Close(); };
-            Button printButton = new Button() { Content = Properties.Resources.Print, Margin = new Thickness(4) };
+            Button printButton = new Button() {Content = Properties.Resources.Print, Margin = new Thickness(4)};
             printButton.Click += ButtonBase_OnClick;
-            Buttons = new Button[] { printButton, customButton };
+            Buttons = new Button[] {printButton, customButton};
 
             for (int i = 0; i < _categories.Count(); i++) {
                 for (int j = 0; j < 2; j++) {
                     DataGrid dataGrid = new DataGrid();
                     dataGrid.Margin = new Thickness(10);
+                    dataGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    dataGrid.PreviewMouseWheel += DataGridOnPreviewMouseWheel;
                     GenerateDatagrid(i, j);
 
                     if (_tables[i, j] != null) {
@@ -72,22 +76,27 @@ namespace Ui.Main.Pages.Competition.Times
             }
         }
 
+        private void DataGridOnPreviewMouseWheel(object sender, MouseWheelEventArgs e) {
+            Panel.ScrollToVerticalOffset(Panel.VerticalOffset - e.Delta);
+        }
+
         private Label getLabel(int i, int j) {
             Label label = new Label() {
                 FontSize = 12,
                 FontWeight = FontWeights.Bold
             };
 
-            label.HorizontalContentAlignment = HorizontalAlignment.Center;
+            //label.HorizontalContentAlignment = HorizontalAlignment.Center;
 
-            label.Content = j == 0 ? _categories.ElementAt(i).CategoryM.Name.Replace('_', ' ') : _categories.ElementAt(i).CategoryF.Name.Replace('_', ' ');
+            label.Content = j == 0
+                ? _categories.ElementAt(i).CategoryM.Name.Replace('_', ' ')
+                : _categories.ElementAt(i).CategoryF.Name.Replace('_', ' ');
 
             return label;
         }
 
         private void GenerateDatagrid(int index, int rowGender) {
-            DataColumn column = new DataColumn(Properties.Resources.AthletePosition, typeof(string))
-            {
+            DataColumn column = new DataColumn(Properties.Resources.AthletePosition, typeof(string)) {
                 AllowDBNull = true
             };
 
@@ -99,7 +108,8 @@ namespace Ui.Main.Pages.Competition.Times
             tableRef.Columns[Properties.Resources.AthletePosition].AutoIncrementSeed = 1;
             tableRef.Columns[Properties.Resources.AthletePosition].AutoIncrementStep = 1;
 
-            tableRef.Merge(_service.SelectCompetitionTimes(_competition, _categories.ElementAt(index), Genders[rowGender]));
+            tableRef.Merge(_service.SelectCompetitionTimes(_competition, _categories.ElementAt(index),
+                Genders[rowGender]));
             if (tableRef.Rows.Count <= 0) {
                 tableRef = null;
                 return;
@@ -119,16 +129,13 @@ namespace Ui.Main.Pages.Competition.Times
             dtClone.Columns[Properties.Resources.Time].DataType = typeof(string);
 
 
-            foreach (DataRow row in tableRef.Rows)
-            {
+            foreach (DataRow row in tableRef.Rows) {
                 object[] dr = row.ItemArray as object[];
-                if (dr[6] is DBNull || dr[7] is DBNull || (long)dr[7] == 0)
-                {
+                if (dr[6] is DBNull || dr[7] is DBNull || (long) dr[7] == 0) {
                     dr[6] = "---";
                 }
-                else
-                {
-                    var seconds = (long)dr[7] - (long)dr[6];
+                else {
+                    var seconds = (long) dr[7] - (long) dr[6];
                     var timespan = TimeSpan.FromSeconds(seconds);
                     dr[6] = timespan.ToString(@"hh\:mm\:ss");
                 }
@@ -150,16 +157,16 @@ namespace Ui.Main.Pages.Competition.Times
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
             Panel.ScrollToTop();
 
-            PrintDialog printDlg = new PrintDialog();
-            printDlg.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
+            PrintDialog printDialog = new PrintDialog();
+            //printDlg.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
+            printDialog.PageRangeSelection = PageRangeSelection.AllPages;
+            printDialog.UserPageRangeEnabled = true;
 
-            var result = printDlg.ShowDialog();
+            var result = printDialog.ShowDialog();
 
             if ((bool) result) {
-                Size pageSize = new Size { Height = printDlg.PrintableAreaHeight, Width = printDlg.PrintableAreaWidth };
-                Panel.Measure(pageSize);
-                Panel.UpdateLayout();
-                printDlg.PrintVisual(Panel, "Tiempos");
+                //Size pageSize = new Size { Height = printDlg.PrintableAreaHeight, Width = printDlg.PrintableAreaWidth };
+                printDialog.PrintVisual(Layout, "Tiempos");
             }
         }
     }
